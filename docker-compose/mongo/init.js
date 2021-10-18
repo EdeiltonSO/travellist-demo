ce220b0004517c897ae3cecca7909f7bf4420c05
Edeilton Oliveira <edeiltonso@gmail.com>
2021-10-18 13:43:29 -0300

    initial commit

---
+++ docker-compose/mongo/init.js
@@ -0,0 +1,16 @@
+// id, name, visited
+use "travellist";
+
+db.places.insert([
+    {name: "Berlin", visited: 0},
+    {name: "Budapest", visited: 0},
+    {name: "Cincinnati", visited: 1},
+    {name: "Denver", visited: 0},
+    {name: "Helsinki", visited: 0},
+    {name: "Lisbon", visited: 0},
+    {name: "Moscow", visited: 1},
+    {name: "Nairobi", visited: 0},
+    {name: "Oslo", visited: 1},
+    {name: "Rio", visited: 0},
+    {name: "Tokyo", visited: 0},
+]);
